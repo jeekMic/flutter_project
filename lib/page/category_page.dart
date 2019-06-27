166a06ef480d83b4a4b29b32a22b2682163c25a0
hongbiao <hongb@suchness.cn>
2019-06-27 15:18:52 +0800

    home_page

--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -1,11 +1,62 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-class CategoryPage extends StatelessWidget {
+import 'package:flutter_shop/service/service_method.dart';
+class CategoryPage extends StatefulWidget {
+  CategoryPage({Key key}) : super(key: key);
+
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  TextEditingController textEditingController = new TextEditingController();
+  String showText = "收到数据信息";
+  @override
+  void initState() {
+    getHomePageContent().then((val){
+          showText = val;
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("分类"),
+
+    return Column(
+        
+        children: <Widget>[
+          RaisedButton(
+            child: Text("点击查询数据"),
+            padding: EdgeInsets.only(top: 100),
+            onPressed: (){
+              //  _require();
+               textEditingController.text = showText;
+            },
+          ),
+        TextField(
+        maxLines: 15,
+        controller: textEditingController,
+        decoration: InputDecoration(
+            hintText: "请输入信息"
+            
+        ),
       ),
+        ],
     );
   }
+  void _require(){
+  getHttpData().then((vale){
+    setState(() {
+     showText = vale;
+    });
+    print(vale);
+      });
+    }
+Future  getHttpData() async{
+  Dio dio = new Dio();
+  var formData ={'lon':'115.12932', 'lat':'35.541'};
+  Response response = await dio.post("http://v.jspang.com:8088/baixing/wxmini/homePageContent",data: formData);
+  print("hb====${response.data}");
+  return response.data;
+    }
 }
+
+
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -6,6 +6,7 @@ import '../service/service_method.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
@@ -14,15 +15,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String homePageContent = "正在读取数据";
-
+  List<Map> swiper;
+  List<Map> bavigatorList;
   @override
   void initState() {
     getHomePageContent().then((val){
-         print("=======31");
       setState(() {
-        print("=======3");
        homePageContent = val.toString(); 
-       print("=======4");
       });
     });
     super.initState();
@@ -30,9 +29,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
-
        child: Scaffold(
          appBar: AppBar(title: Text("百姓生活家"),),
          body: FutureBuilder(
@@ -40,13 +37,18 @@ class _HomePageState extends State<HomePage> {
             builder: (context,snapshot){
               if(snapshot.hasData){
                 var data = json.decode(snapshot.data.toString());
-                List<Map> swiper = (data['data']['slides'] as List).cast();
-                List<Map> bavigatorList = (data['data']['category'] as List).cast();
+                swiper = (data['data']['slides'] as List).cast();
+                bavigatorList = (data['data']['category'] as List).cast();
+                String adPicture = (data["data"]["advertesPicture"]["PICTURE_ADDRESS"]);
+                String leaderImage = data['data']['shopInfo']['leaderImage'];
+                String leaderPhone = data['data']['shopInfo']['leaderPhone'];
                 // List<Map> swiper = swiperimage;
                 return Column(
                   children: <Widget>[
                     SwiperDiy(swiperDataList: swiper),
-                    TopNavigator(bavigatorList:bavigatorList)
+                    TopNavigator(bavigatorList:bavigatorList),
+                    AdBanner(adPicture: adPicture,),
+                    LeaderPhone(leaderImage: leaderImage,leaderPhone: leaderPhone,)
                   ],
                 );
               }else{
@@ -110,6 +112,9 @@ class TopNavigator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if(this.bavigatorList.length>10){
+      this.bavigatorList.removeRange(10,this.bavigatorList.length);
+    }
     return Container(
       height: ScreenUtil().setHeight(320),
       padding: EdgeInsets.all(3.0),
@@ -122,6 +127,44 @@ class TopNavigator extends StatelessWidget {
       ),
     );
   }
+}
+
+
+class AdBanner extends StatelessWidget {
+  final String adPicture;
+  AdBanner({Key key,this.adPicture}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(adPicture),
+    );
+  }
+}
+
+//店长电话
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage;
+  final String leaderPhone;//电话
+  LeaderPhone({Key key,String this.leaderImage,String this.leaderPhone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+      onTap: (){},
+      child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _lanucher() async{
+    String url = "tel"+leaderPhone;
+    if(await canLaunch(url)){
+        await launch(url);
+    }else{
+      throw "url不能进行访问,异常";
+    }
 }
 
 // class HomePage extends StatefulWidget {
